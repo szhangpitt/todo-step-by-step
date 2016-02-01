22bc358e1f7cd0cfa1ff01cec7b723506cebcb45
Shaopeng Zhang <szhang351@bloomberg.net>
2016-02-01 15:47:47 -0500

    render the data to match your initial target html

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,17 @@
 
     $.getJSON('data/data.json', function (todos) {
         console.log(todos);
+
+        $('#todo-list').append(
+            todos.map(function (todo, index) {
+                return '' +
+                    '<li>' +
+                        '<label for="todo-input-' + index + '">' +
+                            '<input type="checkbox" id="todo-input-' + index + '">&nbsp;' +
+                             todo +
+                        '</label>' +
+                    '</li>';
+            }));
     });
         
 }(jQuery));
